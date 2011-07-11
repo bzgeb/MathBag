b7e41da3dce65e23a3b22db0d7809dee8f0904d0
Bronson Zgeb <bronson.zgeb@gmail.com>
2011-07-11 19:43:52 -0400

    Initial Commit

---
+++ MathBag/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MathBag
+//
+//  Created by bronson on 11-07-11.
+//  Copyright 2011 Concordia University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
